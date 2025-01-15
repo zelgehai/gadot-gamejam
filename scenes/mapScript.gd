b6003d913f67107f356e5275639f6096adc7beef
Zelgehai Zahid <zelgehai@gmail.com>
2025-01-14 23:05:16 -0800

    wasd movement added
    w - UP
    a - LEFT
    d - RIGHT
    s - DOWN

--- scenes/mapScript.gd
+++ scenes/mapScript.gd
@@ -3,11 +3,27 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$Player.position.x = 500
+	$Player.position.y = 250
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if $Player.position.x > 1150:
 		print("position exceeded!")
 		$Player.position.x = 0
+	if $Player.position.x < 0:
+		print("Neg Limit Exceeded")
+		$Player.position.x = 1150
+	if $Player.position.y < 0:
+		$Player.position.y = 650
+	if $Player.position.y > 650:
+		$Player.position.y = 0
+
+	if Input.is_action_pressed("left"):
+		$Player.position.x -= 1
+	if Input.is_action_pressed("right"):
+		$Player.position.x += 1
+	if Input.is_action_pressed("down"):
+		$Player.position.y += 1
+	if Input.is_action_pressed("up"):
+		$Player.position.y -= 1
