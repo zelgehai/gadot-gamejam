202fb244c4978b936093b175dae55094a293f815
Zelgehai Zahid <zelgehai@gmail.com>
2025-01-15 23:14:42 -0800

    I added a comment

--- scenes/mini_bolt.gd
+++ scenes/mini_bolt.gd
@@ -6,6 +6,7 @@ var direction: Vector2 = Vector2.UP
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#adding comment
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
