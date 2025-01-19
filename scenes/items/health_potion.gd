513669a080bb2bd5d53012ed8540263aeca5efcc
Zelgehai Zahid <zelgehai@gmail.com>
2025-01-19 13:25:33 -0800

    Alan optimizations

--- scenes/items/health_potion.gd
+++ scenes/items/health_potion.gd
@@ -1,23 +1,16 @@
 extends Area2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
-
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	print("Player picked up Potion")
 	if Globals.health_amount < Globals.max_health_amount:
 		Globals.health_amount += 1
 		$"../../UI".update_health_amount()
 		queue_free()
-		
-		
-func spawn_healthpot():
-	print("spawning Health_potion")
--- scenes/wolfScript.gd
+++ scenes/wolfScript.gd
@@ -5,12 +5,17 @@ var direction_to_player = Vector2(1,0)
 var speed = 100
 var randomized_speed = 1
 
+var power = 1
+var itemType = 1
+var dropChance = 50
+var health = 5
+
 func _ready() -> void:
 	direction_to_player = (player_node.global_position - global_position).normalized()
 	randomized_speed = (randi() % 250 + speed) # Random num from speed -> 500
 	
 func _process(_delta: float) -> void:
-	if player_node:
+	#if player_node:
 		#get directions towards the player [ to know to face it]
 		direction_to_player = (player_node.global_position - global_position).normalized()
 		velocity = direction_to_player * randomized_speed #moves toward player
@@ -19,34 +24,24 @@ func _process(_delta: float) -> void:
 		rotation += deg_to_rad(90)
 
 #calls this func if wolf got hit
-func hit(position):
-	print('wolf got hit and died.')
-	call_deferred("queue_free") #Deletes wolf when hit. Used
-	#Loot Drop TESTING
-	var lootdrop = randi() % 100 + 1
-	print(lootdrop)
-	if (lootdrop <= 50):
-		#loading HP scene
-		var health_potion_scene = preload("res://scenes/items/HealthPotion.tscn")
-		#instance
-		var health_potion_instance = health_potion_scene.instantiate()
-		var items_node = get_tree().get_root().get_node("map/items")
-		items_node.call_deferred("add_child", health_potion_instance) #used this method because of debugger errors
-		#get_tree().get_root().get_node("map").get_node("items").add_child(health_potion_instance)
-		#spawn Potion at last location of wolf
-		health_potion_instance.global_position = position
-		health_potion_instance.spawn_healthpot()
+func hit(dmg):
+	health -= dmg
+	print('wolf got hit')
 	
+	if(health <= 0):
+		print('wolf died.')
+		call_deferred("queue_free") #Deletes wolf when hit. Used
+		Globals.spawn_item(itemType, dropChance, position)
 	
-func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
-	if canDamage:
-		if body.has_method("player_hit"): #can also use "if "hit" in body:"
-			Globals.health_amount -=1
+func _on_area_2d_body_entered(body) -> void:
+	if canDamage and !Globals.Invulnerable:
+		if body.name == "Player":
+			Globals.damage_Player(power)
 			body.player_hit()
 		canDamage = false
 		$Timer.start()
 
-func _on_area_2d_body_exited(_body: CharacterBody2D) -> void:
+func _on_area_2d_body_exited(_body) -> void:
 	pass
 	
 func _on_timer_timeout() -> void:
